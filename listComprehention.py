6a53aea89d687b6aa2057ae8086b2da1da314dac
atul314e <atul.silori@314ecorp.com>
2021-07-05 00:46:18 +0530

    added totalNums variable

--- listComprehention.py
+++ listComprehention.py
@@ -1,6 +1,7 @@
 data = list(range(1200))
 
-Iter=int(1200/500)+1 if int(1200/500)!=(1200/500) else int(1200/500)
+totalNums=len(data)
+Iter=int(totalNums/500)+1 if int(totalNums/500)!=totalNums/500 else totalNums/500
 start=0
 end=500
 chunks = [data[start+500*ite:end+500*ite] if end+500*ite<=1200 else data[start+500*ite:1200] for ite in range(Iter)]
