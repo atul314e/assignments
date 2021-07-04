91ac5d73138b6a6a91bc12a4775d510f4e04a5b1
atul314e <atul.silori@314ecorp.com>
2021-07-05 00:41:08 +0530

    added condition for Iter

--- listComprehention.py
+++ listComprehention.py
@@ -1,6 +1,6 @@
 data = list(range(1200))
 
-Iter=int(1200/500)+1
+Iter=int(1200/500)+1 if int(1200/500)!=(1200/500) else int(1200/500)
 start=0
 end=500
 chunks = [data[start+500*ite:end+500*ite] if end+500*ite<=1200 else data[start+500*ite:1200] for ite in range(Iter)]
