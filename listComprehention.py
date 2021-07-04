36043d0047232f1d35e97ecc37943d6a1dd3efb5
atul314e <atul.silori@314ecorp.com>
2021-07-05 00:15:11 +0530

    added listComprehention.py

---
+++ listComprehention.py
@@ -0,0 +1,10 @@
+data = list(range(1200))
+
+Iter=int(1200/500)+1
+start=0
+end=500
+chunks = [data[start+500*ite:end+500*ite] if end+500*ite<=1200 else data[start+500*ite:1200] for ite in range(Iter)]
+
+assert len(chunks)==3
+assert len(chunks[0])==500
+assert len(chunks[-1])==200
