d6683ac612b2f592c30e069f6d6d9230293b56a2
atul314e <atul.silori@314ecorp.com>
2021-07-05 01:44:24 +0530

    added totalNums in list comprehention

--- listComprehention.py
+++ listComprehention.py
@@ -4,7 +4,7 @@ totalNums=len(data)
 Iter=int(totalNums/500)+1 if int(totalNums/500)!=totalNums/500 else totalNums/500
 start=0
 end=500
-chunks = [data[start+500*ite:end+500*ite] if end+500*ite<=1200 else data[start+500*ite:1200] for ite in range(Iter)]
+chunks = [data[start+500*ite:end+500*ite] if end+500*ite<=totalNums else data[start+500*ite:totalNums] for ite in range(Iter)]
 
 assert len(chunks)==3
 assert len(chunks[0])==500
